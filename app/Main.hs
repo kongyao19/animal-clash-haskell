2db37125f16fcf0f4329cd0170d10408b107ad57
Kong Yao <kongyao12345@gmail.com>
2025-01-09 16:35:02 +0800

    Define datatypes

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+data Animal = Worm | Chicken | Fox | Bear | Dinosaur
+    deriving (Show, Eq, Ord)
+
+data Card = Card {animal :: Animal, quantity :: Int}
+    deriving (Show, Eq)
+
+data Winner = P1 | P2 | Draw
+    deriving Show
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
