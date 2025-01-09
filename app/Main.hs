42f843436cb7c4880f8544727f0d4dd418ed1152
Kong Yao <kongyao12345@gmail.com>
2025-01-09 18:57:10 +0800

    Update functions and create typeclass instances

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,20 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Main where
 
 data Animal = Worm | Chicken | Fox | Bear | Dinosaur
     deriving (Show, Eq, Ord)
 
 data Card = Card {animal :: Animal, quantity :: Int}
-    deriving (Show, Eq)
+    deriving Eq
+
+instance Show Card where
+    show (Card a q)= "Card " ++ show a ++ " " ++ show q
 
 data Winner = P1 | P2 | Draw
-    deriving Show
+
+instance Show Winner where
+    show P1 = "Player 1 has won!"
+    show P2 = "Player 2 has won!"
+    show Draw = "Its a draw!"
 
 type Deck = [Card]
 
@@ -20,18 +26,28 @@ checkDeck c = any isValidCard
     where
         isValidCard (Card a q) = a == animal c && q >= quantity c
 
-chooseCard :: Card -> Deck -> Maybe Card
-chooseCard c d = if checkDeck c d then Just c else Nothing
+updateDeck :: Card -> Deck -> Deck
+updateDeck (Card a q) = filter keepCard . map updateCard
+    where
+        updateCard (Card a' q')
+            | a == a' && q' >= q = Card a' (q' - q)
+            | otherwise = Card a' q'
+        keepCard (Card _ q') = q' > 0
 
-clash :: Card -> Card -> Winner
-clash (Card a1 q1) (Card a2 q2) 
-    | a1 == a2 = case compare q1 q2 of
-        LT -> P2
-        EQ -> Draw
-        GT -> P1
-    | otherwise = case compare a1 a2 of
-        LT -> P2
-        GT -> P1
+chooseCard :: Card -> Deck -> Maybe (Card, Deck)
+chooseCard c d  
+    | checkDeck c d = Just (c, updateDeck c d)
+    | otherwise = Nothing
+
+battle :: Card -> Card -> Winner
+battle (Card a1 q1) (Card a2 q2) 
+    | a1 == a2 = result q1 q2
+    | otherwise = result a1 a2
+    where
+        result x y = case compare x y of
+            LT -> P2
+            EQ -> Draw
+            GT -> P1
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
