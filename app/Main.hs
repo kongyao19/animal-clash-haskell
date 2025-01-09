3a041c22ee733450edd931ce24be1721b67425a1
Kong Yao <kongyao12345@gmail.com>
2025-01-09 16:57:00 +0800

    Add functions

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,19 @@ data Card = Card {animal :: Animal, quantity :: Int}
 
 data Winner = P1 | P2 | Draw
     deriving Show
+    
+battle :: Card -> Card -> Ordering
+battle (Card a1 q1) (Card a2 q2)
+    | a1 == a2 = compare q1 q2
+    | otherwise = compare a1 a2
+
+result :: Ordering -> Winner
+result LT = P2
+result EQ = Draw
+result GT = P1
+
+clash :: Card -> Card -> Winner 
+clash c1 c2 = result (battle c1 c2)
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
